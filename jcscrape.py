989310e987e3e7a5fcb6a5163bbfba30db3e6adf
Jason Keane <jason.keane1996@gmail.com>
2019-06-02 15:58:34 +0100

    Allow URL to be entered as command line argument, cater for Simply Folk show, add songs to Google sheets

--- jcscrape.py
+++ jcscrape.py
@@ -3,6 +3,11 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import spotipy
 import spotipy.util as util
+import datetime
+import sys
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread
+import datetime
 
 ## SPOTIFY AUTH
 username = 'XYZ'
@@ -15,12 +20,38 @@ token = util.prompt_for_user_token(username, scope='playlist-modify-private,play
 sp = spotipy.Spotify(auth=token)
 
 ## BEAUTIFUL SOUP STUFF
-site = 'https://www.rte.ie/radio1/john-creedon/programmes/2019/0411/1042176-john-creedon-thursday-11-april-2019/'
+site = sys.argv[1]
+
 
 page = urlopen(site)
+
 soup = BeautifulSoup(page, 'html.parser')
+
+date_block = soup.find('div', attrs={'class':'fill-blue-radio1 px2 py1 block overflow-hidden text-white mb1'})
+date_par = date_block.find('p', attrs={'class':'mb0'})
+date_string = date_par.find('span', attrs={'class':'bold'}).text
+
 music_played_div = soup.find('div', attrs={'class':'m32-music-played-on-show my2'})
 music_rows = music_played_div.findAll('div', attrs={'class':'small-12 columns border-bottom fill-white p2'})
+
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+credentials = ServiceAccountCredentials.from_json_keyfile_name('Speedtest-4d674b2c34b4.json', scope)
+
+gc = gspread.authorize(credentials)
+
+
+date = ''
+if "john-creedon" in site:
+    playlist_id = '15YBqI1I4n5hsDaM6LiNja'
+    date = date_string.replace('John Creedon ', '')
+    worksheet = gc.open('John Creedon Show Song List').sheet1
+elif "simply-folk" in site:
+    playlist_id = '5lrJMH0WmK5Feh7J5FwrGc'
+    date = date_string.replace('Simply Folk ', '')
+    worksheet = gc.open('Simply Folk Song List').sheet1
+
+
+print(date)
 
 tracks = []
 for music_row in music_rows:
@@ -28,14 +59,16 @@ for music_row in music_rows:
     song = songHTML.find('p', attrs={'class':'mb1'}).text
     artist = songHTML.find('p', attrs={'class':'mb1 bold'}).text
 
+    worksheet.append_row([date, artist, song])
+
     ## Spotify search, query "song artist", gets the top result. This is obviouly not 100% accurate all of the time
     query = song + " " + artist
     results = sp.search(q=query, type='track', limit=1)
-    
+
     for i, t in enumerate(results['tracks']['items']):
         print("Title: " + song + " Artist: " + artist + " Spotify ID: " + t['id'])
         ## Add spotify id to list
-        tracks.append(t['id']) 
+        tracks.append(t['id'])
 
 ## Add tracks to playlist
 sp.user_playlist_add_tracks(user=username, playlist_id=playlist_id, tracks=tracks)
